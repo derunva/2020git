25a56a982b4992fc0d09b32e5524afec1d4440e9
derunva <derun.va@gmail.com>
2020-02-25 15:55:12 +0200

    cylinder fn

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function cube (edge) {
   return edge ** 3
 }
 console.log(cube(10));
+
+function circle (radius) {
+  return 3.14 * ( radius ** 2 )
+}
+console.log(circle(2));
+function cylinder (radius, height) {
+  return circle(radius) * height
+}
+
+console.log(cylinder(5, 10));
